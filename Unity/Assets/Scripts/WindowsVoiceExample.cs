74e61dd73d03674d96bd1e792ed98939c6083d66
sewonist <sewonist@gmail.com>
2019-11-27 20:13:40 +0900

    Add Unity example

---
+++ Unity/Assets/Scripts/WindowsVoiceExample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsVoiceExample : MonoBehaviour
+{
+    public void Run()
+    {
+        WindowsVoice.speak("동해물과 백두산이 마르고 닳도록");
+    }
+}
